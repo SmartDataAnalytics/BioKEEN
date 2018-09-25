78e9652ead5f7eaaace2b9714d0b0358be3fa17c
Mehdi Ali <ali-mehdi@live.de>
2018-09-25 17:37:25 +0200

    Setup and refactor projects

--- src/biokeen/pipeline.py
+++ src/biokeen/pipeline.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# TODO mehdi do this
+"""
+
+1. Hard code all parameters for running the TransE model
+2. run it here
+
+"""
+
+import json
+import os
+
+from biokeen.constants import DATA_DIR
+from keen.run import run
+
+CONFIG_PATH = os.path.join(DATA_DIR, "configuration.json")
+
+
+def main():
+    with open(CONFIG_PATH) as file:
+        config = json.load(file)
+
+    run(config)
+
+
+if __name__ == '__main__':
+    main()
