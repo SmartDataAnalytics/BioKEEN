34c24a3b0116854610fe74ccae3681db6ff20f2e
Mehdi Ali <ali-mehdi@live.de>
2018-11-21 15:55:21 +0100

    New release

--- src/biokeen/constants.py
+++ src/biokeen/constants.py
@@ -9,7 +9,7 @@ DATA_DIR_ENVVAR = 'BioKEEN_DATA'
 DATA_DIR = os.environ.get(DATA_DIR_ENVVAR) or os.path.abspath(os.path.join(HERE, os.pardir, os.pardir, 'data'))
 os.makedirs(DATA_DIR, exist_ok=True)
 
-VERSION = '0.0.5-dev'
+VERSION = '0.0.5'
 
 CONFIG_PATH = os.path.join(DATA_DIR, "configuration.json")
 EMOJI = '🍩'
