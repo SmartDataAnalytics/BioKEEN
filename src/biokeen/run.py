27e19863c9f600157a6119d2c80224b560a5c944
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-01-20 17:31:41 +0100

    Remove unnecessary redefinitions

--- src/biokeen/run.py
+++ src/biokeen/run.py
@@ -2,28 +2,9 @@
 
 """Script for starting the pipeline and saving the results."""
 
-from dataclasses import dataclass
-from typing import Dict, Mapping, Optional
+from pykeen.run import Results, run
 
-import pykeen
-from pykeen.utilities.pipeline import Pipeline
-
-
-@dataclass
-class Results:
-    """Results from PyKEEN."""
-
-    config: Mapping
-    pipeline: Pipeline
-    results: Mapping
-
-
-def run(config: Dict,
-        output_directory: Optional[str] = None) -> Results:
-    """Run PyKEEN using a given configuration."""
-    results = pykeen.run(
-        config=config,
-        output_directory=output_directory,
-    )
-
-    return results
+__all__ = [
+    'Results',
+    'run',
+]
