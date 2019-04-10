63000148646c45513ffe70009cfca8304ca1cbcd
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-04-10 19:34:30 +0200

    Update __init__.py

--- src/biokeen/__init__.py
+++ src/biokeen/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""BioKEEN."""
+"""A package for training and evaluating knowledge graph embedding models on biological knowledge graphs."""
 
-from . import convert
+from . import convert  # noqa: 401
