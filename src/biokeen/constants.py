b6651cc1f305d6f7c890ed3cffcb7b1d74d28c1e
mali <ali-mehdi@live.de>
2019-04-18 13:21:53 +0200

    Bump version: 0.0.13 → 0.0.14-dev

--- src/biokeen/constants.py
+++ src/biokeen/constants.py
@@ -36,7 +36,7 @@ class BiokeenConfig(easy_config.EasyConfig):
 biokeen_config = BiokeenConfig.load()
 os.makedirs(biokeen_config.data_directory, exist_ok=True)
 
-VERSION = '0.0.13'
+VERSION = '0.0.14-dev'
 EMOJI = '🍩'
 
 # Available databases
